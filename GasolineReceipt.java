2b91a26f8b5ef8e92dcb4e458adc274acd34570e
falcon <aaron.skilldistill@gmail.com>
2017-09-07 13:39:46 -0600

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,17 @@
+public class GasolineReceipt {
+  public static void main( String[] args ) {
+    System.out.println("+--------------------------+");
+    System.out.println("|                          |");
+    System.out.println("|       CORNER STORE       |");
+    System.out.println("|                          |");
+    System.out.println("|  2017-09-7   12:44PM     |");
+    System.out.println("|                          |");
+    System.out.println("|  Gallons:       12.00    |");
+    System.out.println("|  Price/gallon: $ 2.50    |");
+    System.out.println("|                          |");
+    System.out.println("|                          |");
+    System.out.println("|  Fuel total:  $ 30.00    |");
+    System.out.println("|                          |");
+    System.out.println("+--------------------------+");
+  }
+}
